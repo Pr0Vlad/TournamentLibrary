498d762e8b9bafa5c34b05754a8d5c7bf08c972b
Vladislav Smirnov <55094161+Pr0Vlad@users.noreply.github.com>
2020-06-16 12:29:40 -1000

    making forms for UI

---
+++ TournamentUI/TournamentViewer.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TournamentUI
+{
+    partial class TournamentViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HeaderLabel = new System.Windows.Forms.Label();
+            this.TournamentNameLabel = new System.Windows.Forms.Label();
+            this.RoundLabel = new System.Windows.Forms.Label();
+            this.RoundDropDown = new System.Windows.Forms.ComboBox();
+            this.UnplayedCheckBox = new System.Windows.Forms.CheckBox();
+            this.MatchupListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // HeaderLabel
+            // 
+            this.HeaderLabel.AutoSize = true;
+            this.HeaderLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeaderLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.HeaderLabel.Location = new System.Drawing.Point(12, 9);
+            this.HeaderLabel.Name = "HeaderLabel";
+            this.HeaderLabel.Size = new System.Drawing.Size(197, 45);
+            this.HeaderLabel.TabIndex = 0;
+            this.HeaderLabel.Text = "Tournament:";
+            this.HeaderLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TournamentNameLabel
+            // 
+            this.TournamentNameLabel.AutoSize = true;
+            this.TournamentNameLabel.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TournamentNameLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.TournamentNameLabel.Location = new System.Drawing.Point(204, 14);
+            this.TournamentNameLabel.Name = "TournamentNameLabel";
+            this.TournamentNameLabel.Size = new System.Drawing.Size(121, 40);
+            this.TournamentNameLabel.TabIndex = 1;
+            this.TournamentNameLabel.Text = "<none>";
+            this.TournamentNameLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // RoundLabel
+            // 
+            this.RoundLabel.AutoSize = true;
+            this.RoundLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RoundLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.RoundLabel.Location = new System.Drawing.Point(13, 89);
+            this.RoundLabel.Name = "RoundLabel";
+            this.RoundLabel.Size = new System.Drawing.Size(94, 37);
+            this.RoundLabel.TabIndex = 2;
+            this.RoundLabel.Text = "Round";
+            // 
+            // RoundDropDown
+            // 
+            this.RoundDropDown.FormattingEnabled = true;
+            this.RoundDropDown.Location = new System.Drawing.Point(113, 97);
+            this.RoundDropDown.Name = "RoundDropDown";
+            this.RoundDropDown.Size = new System.Drawing.Size(212, 29);
+            this.RoundDropDown.TabIndex = 3;
+            // 
+            // UnplayedCheckBox
+            // 
+            this.UnplayedCheckBox.AutoSize = true;
+            this.UnplayedCheckBox.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UnplayedCheckBox.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.UnplayedCheckBox.Location = new System.Drawing.Point(113, 132);
+            this.UnplayedCheckBox.Name = "UnplayedCheckBox";
+            this.UnplayedCheckBox.Size = new System.Drawing.Size(212, 41);
+            this.UnplayedCheckBox.TabIndex = 4;
+            this.UnplayedCheckBox.Text = "Unplayed Only";
+            this.UnplayedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MatchupListBox
+            // 
+            this.MatchupListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MatchupListBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MatchupListBox.FormattingEnabled = true;
+            this.MatchupListBox.ItemHeight = 30;
+            this.MatchupListBox.Location = new System.Drawing.Point(20, 201);
+            this.MatchupListBox.Name = "MatchupListBox";
+            this.MatchupListBox.Size = new System.Drawing.Size(305, 152);
+            this.MatchupListBox.TabIndex = 5;
+            // 
+            // TournamentViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(946, 544);
+            this.Controls.Add(this.MatchupListBox);
+            this.Controls.Add(this.UnplayedCheckBox);
+            this.Controls.Add(this.RoundDropDown);
+            this.Controls.Add(this.RoundLabel);
+            this.Controls.Add(this.TournamentNameLabel);
+            this.Controls.Add(this.HeaderLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "TournamentViewer";
+            this.Text = "Tournament Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeaderLabel;
+        private System.Windows.Forms.Label TournamentNameLabel;
+        private System.Windows.Forms.Label RoundLabel;
+        private System.Windows.Forms.ComboBox RoundDropDown;
+        private System.Windows.Forms.CheckBox UnplayedCheckBox;
+        private System.Windows.Forms.ListBox MatchupListBox;
+    }
+}
+
