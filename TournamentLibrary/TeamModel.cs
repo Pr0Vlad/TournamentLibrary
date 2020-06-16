f3a5e95b531760e3461c812aac6bd85f0d6251fc
Vladislav Smirnov <55094161+Pr0Vlad@users.noreply.github.com>
2020-06-15 23:28:38 -1000

    making more classes for data

--- TournamentLibrary/TeamModel.cs
+++ TournamentLibrary/TeamModel.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace TournamentLibrary
 {
-    class TeamModel
+    public class TeamModel
     {
+        public List<Person> TeamMembers { get; set; }
+        public string TeamName { get; set; }
+
+        public TeamModel()
+        {
+            TeamMembers = new List<Person>();
+        }
+
     }
 }
---
+++ TournamentLibrary/TournamentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentLibrary
+{
+    public class TournamentModel
+    {
+        public string TournamentName { get; set }
+        public decimal EntryFee { get; set }
+        public List<TeamModel> EnteredTeams { get; set; }
+        public List<PrizeModel> Prizes { get; set; }
+        public List<List<MatchupModel>> Rounds { get; set; }
+
+        public TournamentModel()
+        {
+            EnteredTeams = new List<TeamModel>();
+
+            Prizes = new List<PrizeModel>();
+
+            Rounds = new List<List<MatchupModel>>();
+        }
+    }
+}
