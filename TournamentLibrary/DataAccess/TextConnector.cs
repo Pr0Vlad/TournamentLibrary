600b8bc0a57c80a33b96340966e8863f3476fd35
Vladislav Smirnov <55094161+Pr0Vlad@users.noreply.github.com>
2020-06-24 18:36:29 -1000

    reading and writing to a text file the models.

--- TournamentLibrary/DataAccess/TextConnector.cs
+++ TournamentLibrary/DataAccess/TextConnector.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using TournamentLibrary.Models;
+using TournamentLibrary.DataAccess.TextHelper;
+using System.Linq;
 
 namespace TournamentLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
         public PrizeModel CreatePrize(PrizeModel model)
-        {
-            model.Id = 1;
+        {   
+            //loading textfile and converting it to a text List<orizemodel>
+            List<PrizeModel> prizes = PrizesFile.fullFilePath().LoadFile().ConvertToPrizeModels();
 
-            return model;
+            //find max id
+            int currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            model.Id = currentId;
+            currentId += 1;
+
+            //adding new record with new id
+            prizes.Add(model);
+
+            //converting models and added models back into text and saving as text file/updating
+            prizes.WriteToPrizeFile(PrizesFile);
+
         }
     }
 }
